17e65f5083b993aa15b5a54212343afb154bb950
Matthew Snape <Matt.Snape@warwick.ac.uk>
2022-11-07 23:42:41 +0000

    CaesarCipher test should be working

--- src/Testing/testCaesarCipher.cpp
+++ src/Testing/testCaesarCipher.cpp
@@ -8,12 +8,11 @@ TEST_CASE("encryption is applied, [alphanumeric]"){
     const std::string abc{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     const size_t size = abc.size();
     std::string res{""};
-    std::string shfstr{""};
     size_t shift{0};
     CipherMode mode = CipherMode::encrypt;
-    for (size_t key = 0 ; key < 26 ; key++){
+    for (size_t key = 0 ; key < 25 ; key++){
         CaesarCipher test(key);
-        for (size_t i ; i < size ; i++ ){
+        for (size_t i = 1 ; i < size ; i++ ){
             shift = ( i + key ) % size;
             std::string input {abc[i]};
             res = test.applyCipher(input , mode);
@@ -27,17 +26,16 @@ TEST_CASE("decryption is applied, [alphanumeric]"){
     const std::string abc{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     const size_t size = abc.size();
     std::string res{""};
-    std::string shfstr{""};
     size_t shift{0};
-    CipherMode mode = CipherMode::encrypt;
-    for (size_t key = 0 ; key < 26 ; key++){
+    CipherMode mode = CipherMode::decrypt;
+    for (size_t key = 0 ; key < 25 ; key++){
         CaesarCipher test(key);
-        for (size_t i ; i < size ; i++ ){
-            shift = ( i - key ) % size;
+        for (size_t i = 0 ; i < size ; i++ ){
+            shift = ( i + size - key ) % size;
             std::string input {abc[i]};
             res = test.applyCipher(input , mode);
-            std::string exp{abc[shift]};
-            REQUIRE( res == exp );            
+            std::string comp{abc[shift]};
+            REQUIRE( res == comp );            
         }       
     }
 }
