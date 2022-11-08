c3ff00a9daf730c82f6c7e7498a73e3b9e631128
Matthew Snape <Matt.Snape@warwick.ac.uk>
2022-11-08 20:19:23 +0000

    Testing for Command Line incomplete, Version/key broken

--- src/Testing/testCaesarCipher.cpp
+++ src/Testing/testCaesarCipher.cpp
@@ -4,38 +4,42 @@
 #include "CaesarCipher.hpp"
 #include "CipherMode.hpp"
 
-TEST_CASE("encryption is applied, [alphanumeric]"){
+TEST_CASE("CaesarCipher Works, [alphanumeric]"){
     const std::string abc{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     const size_t size = abc.size();
     std::string res{""};
     size_t shift{0};
-    CipherMode mode = CipherMode::encrypt;
+    std::string input {""};
+
+    SECTION("Encryption is applied, [alphanumeric]"){
+    CipherMode en = CipherMode::encrypt;
     for (size_t key = 0 ; key < 25 ; key++){
         CaesarCipher test(key);
         for (size_t i = 1 ; i < size ; i++ ){
             shift = ( i + key ) % size;
-            std::string input {abc[i]};
-            res = test.applyCipher(input , mode);
+            input = {abc[i]};
+            res = test.applyCipher(input , en);
             std::string exp{abc[shift]};
             REQUIRE( res == exp );            
-        }       
+        }     
+    }
+
+    SECTION("Decryption is applied, [alphanumeric]"){
+    CipherMode dec = CipherMode::decrypt;
+    for (size_t key = 0 ; key < 25 ; key++){
+        CaesarCipher test(key);
+        for (size_t i = 1 ; i < size ; i++ ){
+            shift = ( i + size - key ) % size;
+            input = {abc[i]};
+            res = test.applyCipher(input , dec);
+            std::string exp{abc[shift]};
+            REQUIRE( res == exp );            
+        }     
+    }
+
     }
 }
 
-TEST_CASE("decryption is applied, [alphanumeric]"){
-    const std::string abc{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    const size_t size = abc.size();
-    std::string res{""};
-    size_t shift{0};
-    CipherMode mode = CipherMode::decrypt;
-    for (size_t key = 0 ; key < 25 ; key++){
-        CaesarCipher test(key);
-        for (size_t i = 0 ; i < size ; i++ ){
-            shift = ( i + size - key ) % size;
-            std::string input {abc[i]};
-            res = test.applyCipher(input , mode);
-            std::string comp{abc[shift]};
-            REQUIRE( res == comp );            
-        }       
-    }
 }
+
+
---
+++ src/Testing/testCommandLine.cpp
@@ -0,0 +1,32 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "ProcessCommandLine.hpp"
+
+#include <vector>
+
+TEST_CASE("Testing Command line,[alphanumeric]"){
+    ProgramSettings test_set;
+
+    SECTION("Testing file input/output"){
+        processCommandLine(std::vector<std::string>{"","-i" , "in_test.txt","-o","out_test.txt","-h","--version","-k","5"},test_set);
+        REQUIRE(test_set.inputFile == std::string{"in_test.txt"});
+        REQUIRE(test_set.outputFile == std::string{"out_test.txt"});
+        REQUIRE(test_set.helpRequested == true);
+        REQUIRE(test_set.versionRequested == true);
+        REQUIRE(test_set.cipherKey == "5");
+    }
+
+    SECTION("Testing decrypt"){
+    processCommandLine(std::vector<std::string>{"","--decrypt"},test_set);
+    REQUIRE(test_set.mode == CipherMode::decrypt);
+    }
+
+    SECTION("Testing encrypt"){
+    processCommandLine(std::vector<std::string>{"","--encrypt"},test_set);
+    REQUIRE(test_set.mode == CipherMode::encrypt);
+    }
+
+
+
+}
