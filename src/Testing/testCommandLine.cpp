cf7a95cae2bca6c224d9776b7ac17feab25ad084
Matthew Snape <Matt.Snape@warwick.ac.uk>
2022-11-09 00:36:02 +0000

    testcommandline mostly working. need to fix test for version and cipher key

--- src/Testing/testCommandLine.cpp
+++ src/Testing/testCommandLine.cpp
@@ -9,11 +9,9 @@ TEST_CASE("Testing Command line,[alphanumeric]"){
     ProgramSettings test_set;
 
     SECTION("Testing file input/output"){
-        processCommandLine(std::vector<std::string>{"","-i" , "in_test.txt","-o","out_test.txt","-h","--version","-k","5"},test_set);
+        processCommandLine(std::vector<std::string>{"","-i" , "in_test.txt","-o","out_test.txt"},test_set);
         REQUIRE(test_set.inputFile == std::string{"in_test.txt"});
         REQUIRE(test_set.outputFile == std::string{"out_test.txt"});
-        REQUIRE(test_set.helpRequested == true);
-        REQUIRE(test_set.versionRequested == true);
         REQUIRE(test_set.cipherKey == "5");
     }
 
@@ -27,6 +25,18 @@ TEST_CASE("Testing Command line,[alphanumeric]"){
     REQUIRE(test_set.mode == CipherMode::encrypt);
     }
 
+    SECTION("Testing help/version/key"){
+    processCommandLine(std::vector<std::string>{"","-h","--version","-k","5"},test_set);
+    REQUIRE(test_set.helpRequested == true);
+    REQUIRE(test_set.versionRequested == true);
+    REQUIRE(test_set.cipherKey == "5");
 
+    }
+
+    SECTION("Additional help test"){
+    processCommandLine(std::vector<std::string>{"","--help"},test_set);
+    REQUIRE(test_set.helpRequested == true);
+
+    }
 
 }
